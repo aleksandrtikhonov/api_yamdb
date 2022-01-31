e112fe8dfefbd30d7d098c14e6254a868e040999
Aleksandr Tikhonov <aleksandrtikhonov91@gmail.com>
2022-01-31 22:05:03 +0300

    fix settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,6 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'reviews',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -48,9 +51,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'api',
-                'reviews',
-                'rest_framework'
             ],
         },
     },
