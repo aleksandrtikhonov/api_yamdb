93f4aba7e44fa6ef2ebdd9883df561a2195cc277
camillaten <ten.camilla@gmail.com>
2022-02-01 14:12:29 +0300

    Переопределена модель User

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'username',
+        'email',
+        'role',
+        'bio',
+        'first_name',
+        'last_name'
+    )
+    list_editable = ('role',)
+    search_fields = ('username', 'email')
+    list_filter = ('role',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(User, UserAdmin)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    USER_ROLES = [
+        ('user', 'Аутентифицированный пользователь'),
+        ('moderator', 'Модератор'),
+        ('admin', 'Администратор'),
+    ]
+    role = models.CharField(
+        max_length=9,
+        choices=USER_ROLES,
+    )
+    bio = models.TextField()
